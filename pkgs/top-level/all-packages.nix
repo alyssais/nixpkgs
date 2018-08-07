c783284991378febc227562ef01d894cc6dc2962
Alyssa Ross <hi@alyssa.is>
2018-08-07 15:24:40 +0100

    triton: init at 6.1.2

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -5527,6 +5527,8 @@ with pkgs;
 
   trickle = callPackage ../tools/networking/trickle {};
 
+  inherit (nodePackages) triton;
+
   triggerhappy = callPackage ../tools/inputmethods/triggerhappy {};
 
   trousers = callPackage ../tools/security/trousers { };
