275b9f56b82254ed7f233e841a70f0746f92a3b8
Alyssa Ross <hi@alyssa.is>
2020-12-02 05:12:42 +0000

    choose: convert to importCargo

--- pkgs/tools/text/choose/default.nix
+++ pkgs/tools/text/choose/default.nix
@@ -1,6 +1,7 @@
 { stdenv
 , fetchFromGitHub
 , rustPlatform
+, importCargo
 }:
 
 rustPlatform.buildRustPackage rec {
@@ -14,7 +15,7 @@ rustPlatform.buildRustPackage rec {
     sha256 = "0j3861pxqw0lnamb201c7h5w7npzyiwwb6c1xzxjv72m2ccvz76j";
   };
 
-  cargoSha256 = "1p18926pfff1yayb2i28v0nz37j52hqqv7244yfrzgidi29kyvbc";
+  nativeBuildInputs = [ (importCargo ./Cargo.lock) ];
 
   meta = with stdenv.lib; {
     description = "A human-friendly and fast alternative to cut and (sometimes) awk";
