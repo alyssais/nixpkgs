68e478a4515fc2fc7a7dc09f5c42aa3cf7a40136
Alyssa Ross <hi@alyssa.is>
2020-12-02 04:07:16 +0000

    dijo: convert to importCargo

--- pkgs/tools/misc/dijo/default.nix
+++ pkgs/tools/misc/dijo/default.nix
@@ -1,16 +1,20 @@
-{ stdenv, lib, rustPlatform, fetchFromGitHub, ncurses, CoreServices }:
-let version = "0.2.3"; in
-rustPlatform.buildRustPackage {
+{ stdenv, lib, rustPlatform, importCargo, fetchFromGitHub
+, ncurses, CoreServices
+}:
+
+rustPlatform.buildRustPackage rec {
   pname = "dijo";
-  inherit version;
-  buildInputs = [ ncurses ] ++ lib.optional stdenv.isDarwin CoreServices;
+  version = "0.2.3";
+
   src = fetchFromGitHub {
     owner = "NerdyPepper";
     repo = "dijo";
     rev = "v${version}";
     sha256 = "1lcvj0pri5v64zygkf2p24vr72y39agrq1r3kb8dfgz8yy3vcz0a";
   };
-  cargoSha256 = "0pm048xf8hkva8q8fjmhrdnk7h2im28ix7xy784xwkkdnilm4j7f";
+
+  nativeBuildInputs = [ (importCargo ./Cargo.lock) ];
+  buildInputs = [ ncurses ] ++ lib.optional stdenv.isDarwin CoreServices;
 
   meta = with lib; {
     description = "Scriptable, curses-based, digital habit tracker";
