23f1a269613028844e0178239de9ebf7187f2022
Alyssa Ross <hi@alyssa.is>
2020-12-24 22:58:42 +0000

    jwt-cli: convert to importCargo

--- pkgs/tools/security/jwt-cli/default.nix
+++ pkgs/tools/security/jwt-cli/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchFromGitHub, rustPlatform, Security, fetchpatch }:
+{ stdenv, fetchFromGitHub, rustPlatform, importCargo, Security, fetchpatch }:
 
 rustPlatform.buildRustPackage rec {
   pname = "jwt-cli";
@@ -11,8 +11,7 @@ rustPlatform.buildRustPackage rec {
     sha256 = "07mnkr7hi29fyyyn7llb30p4ndiqz4gf1lnhm44qm09alaxmfvws";
   };
 
-  cargoSha256 = "0jkzy7ssg9v9phhlldq6s4rfs3sn17y2r1k0jr10g9j15lzixa04";
-
+  nativeBuildInputs = [ (importCargo ./Cargo.lock) ];
   buildInputs = stdenv.lib.optional stdenv.isDarwin Security;
 
   doInstallCheck = true;
