e618bddaf94f4d86977b3f6b6ebe933c7a4a8ed4
Alyssa Ross <hi@alyssa.is>
2020-12-25 03:47:49 +0000

    mdbook: convert to importCargo

--- pkgs/tools/text/mdbook/default.nix
+++ pkgs/tools/text/mdbook/default.nix
@@ -11,8 +11,6 @@ rustPlatform.buildRustPackage rec {
     sha256 = "0nqr5a27i91m71fhpycf60q54qplc920y1fmk9hav3pbb9wcc5dl";
   };
 
-  cargoSha256 = "1p72iwl9ca7a92nf6wyjjbn0qns0xxb4xrbz2r2nmd83cxs0fplg";
-
   nativeBuildInputs = [ (importCargo ./Cargo.lock) ];
   buildInputs = stdenv.lib.optionals stdenv.isDarwin [ CoreServices ];
 
