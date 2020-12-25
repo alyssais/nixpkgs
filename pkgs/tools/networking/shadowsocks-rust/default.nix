128d0756fd8d20b4c2b463ae18d004a33c412948
Alyssa Ross <hi@alyssa.is>
2020-12-25 07:30:12 +0000

    shadowsocks-rust: convert to importCargo

--- pkgs/tools/networking/shadowsocks-rust/default.nix
+++ pkgs/tools/networking/shadowsocks-rust/default.nix
@@ -1,4 +1,6 @@
-{ stdenv, fetchFromGitHub, rustPlatform, pkgconfig, openssl, libsodium, Security }:
+{ stdenv, fetchFromGitHub, rustPlatform, importCargo
+, pkgconfig, openssl, libsodium, Security
+}:
 
 rustPlatform.buildRustPackage rec {
   pname = "shadowsocks-rust";
@@ -11,13 +13,11 @@ rustPlatform.buildRustPackage rec {
     sha256 = "1ylasv33478cgwmr8wrd4705azfzrw495w629ncynamv7z17w3k3";
   };
 
-  cargoSha256 = "060k2dil38bx4zb5nnkr3mj6aayginbhr3aqjv0h071q0vlvp05p";
-
   SODIUM_USE_PKG_CONFIG = 1;
 
   buildInputs = [ openssl libsodium ]
     ++ stdenv.lib.optionals stdenv.isDarwin [ Security ];
-  nativeBuildInputs = [ pkgconfig ];
+  nativeBuildInputs = [ pkgconfig (importCargo ./Cargo.lock) ];
 
   meta = with stdenv.lib; {
     homepage = "https://github.com/shadowsocks/shadowsocks-rust";
