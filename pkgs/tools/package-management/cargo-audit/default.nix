cb61eb039546153e4be0d209d02225e4fe9bb058
Alyssa Ross <hi@alyssa.is>
2020-12-25 03:24:28 +0000

    cargo-audit: convert to importCargo

--- pkgs/tools/package-management/cargo-audit/default.nix
+++ pkgs/tools/package-management/cargo-audit/default.nix
@@ -1,4 +1,7 @@
-{ stdenv, lib, rustPlatform, fetchFromGitHub, openssl, pkg-config, Security, libiconv }:
+{ stdenv, lib, rustPlatform, fetchFromGitHub, importCargo
+, openssl, pkg-config, Security, libiconv
+}:
+
 rustPlatform.buildRustPackage rec {
   pname = "cargo-audit";
   version = "0.13.1";
@@ -10,10 +13,8 @@ rustPlatform.buildRustPackage rec {
     sha256 = "1q8i2c3f8ir1pxkvla4dshz7n0cl97mjydc64xis5pph39f69yc1";
   };
 
-  cargoSha256 = "1b62mcj4gjz2arjf3r30k9qcg81i5x0149a4bfj39xdrz55gg0wh";
-
   buildInputs = [ openssl libiconv ] ++ lib.optionals stdenv.isDarwin [ Security ];
-  nativeBuildInputs = [ pkg-config ];
+  nativeBuildInputs = [ pkg-config (importCargo ./Cargo.lock) ];
 
   # The tests require network access which is not available in sandboxed Nix builds.
   doCheck = false;
