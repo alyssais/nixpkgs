c6a8c33aef744c03112a9134d885fe3d9a6aa5b7
Alyssa Ross <hi@alyssa.is>
2020-12-25 07:03:21 +0000

    mozwire: convert to importCargo

--- pkgs/tools/networking/mozwire/default.nix
+++ pkgs/tools/networking/mozwire/default.nix
@@ -1,4 +1,4 @@
-{ rustPlatform, stdenv, fetchFromGitHub, Security }:
+{ rustPlatform, stdenv, fetchFromGitHub, importCargo, Security }:
 
 rustPlatform.buildRustPackage rec {
   pname = "MozWire";
@@ -11,9 +11,8 @@ rustPlatform.buildRustPackage rec {
     sha256 = "01bj3c34x9ywxygsz4rdyw5gc9cz8x6zzl5fd7db8qy8bx2lhlr9";
   };
 
+  nativeBuildInputs = [ (importCargo ./Cargo.lock) ];
   buildInputs = stdenv.lib.optionals stdenv.isDarwin [ Security ];
-
-  cargoSha256 = "0yxnpnxwis46wz4j5rjzyyzrvh94hn8vzxmmrcmrdz3gkakg77hg";
 
   meta = with stdenv.lib; {
     description = "MozillaVPN configuration manager giving Linux, macOS users (among others), access to MozillaVPN";
