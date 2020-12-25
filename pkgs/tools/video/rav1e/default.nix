09d5e255d40ae27d013e72903146cb8a42cb8623
Alyssa Ross <hi@alyssa.is>
2020-12-25 01:32:45 +0000

    rav1e: convert to importCargo

--- pkgs/tools/video/rav1e/default.nix
+++ pkgs/tools/video/rav1e/default.nix
@@ -1,33 +1,17 @@
-{ stdenv, rustPlatform, fetchurl, fetchFromGitHub, lib, nasm, cargo-c }:
+{ rustPlatform, fetchFromGitHub, lib, importCargo, nasm, cargo-c }:
 
 rustPlatform.buildRustPackage rec {
   pname = "rav1e";
   version = "0.4.0-alpha";
 
-  src = stdenv.mkDerivation rec {
-    name = "${pname}-${version}-source";
-
-    src = fetchFromGitHub {
-      owner = "xiph";
-      repo = "rav1e";
-      rev = "v${version}";
-      sha256 = "1fw1gxi8330kfhl9hfzpn0lcmyn5604lc74d6g6iadzz2hmv4mb9";
-    };
-
-    cargoLock = fetchurl {
-      url = "https://github.com/xiph/rav1e/releases/download/v0.4.0-alpha/Cargo.lock";
-      sha256 = "002s2wlzpifn5p2ahdrjdkjl48c1wr6fslg0if4gf9qpl8qj05fl";
-    };
-
-    installPhase = ''
-      mkdir -p $out
-      cp -r ./* $out/
-      cp ${cargoLock} $out/Cargo.lock
-    '';
+  src = fetchFromGitHub {
+    owner = "xiph";
+    repo = "rav1e";
+    rev = "v${version}";
+    sha256 = "1fw1gxi8330kfhl9hfzpn0lcmyn5604lc74d6g6iadzz2hmv4mb9";
   };
 
-  cargoSha256 = "1i5ldqb77rrhfxxf9krp7f6yj3h6rsqak6hf23fd2znhgmi7psb1";
-  nativeBuildInputs = [ nasm cargo-c ];
+  nativeBuildInputs = [ nasm cargo-c (importCargo ./Cargo.lock) ];
 
   postBuild = ''
     cargo cbuild --release --frozen --prefix=${placeholder "out"}
