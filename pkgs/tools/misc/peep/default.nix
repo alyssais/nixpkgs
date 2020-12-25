a2ad8e0e91ee142df337e35208c087d3895e0ea8
Alyssa Ross <hi@alyssa.is>
2020-12-25 04:32:15 +0000

    peep: convert to importCargo

--- pkgs/tools/misc/peep/default.nix
+++ pkgs/tools/misc/peep/default.nix
@@ -1,4 +1,4 @@
-{ lib, rustPlatform, fetchFromGitHub }:
+{ lib, rustPlatform, fetchFromGitHub, importCargo }:
 
 rustPlatform.buildRustPackage rec {
   pname = "peep";
@@ -11,8 +11,7 @@ rustPlatform.buildRustPackage rec {
     sha256 = "0c0fphnhq9vg9jjnkl35k56jbcnyz2ballsnkbm2xrh8vbyvk1av";
   };
 
-  cargoPatches = [ ./0001-Add-Cargo.lock-by-running-cargo-vendor.patch ];
-  cargoSha256 = "15qc9a4zpnq7lbcaji1mkik93qkx366misczbi1mipiq5w7sgn0l";
+  nativeBuildInputs = [ (importCargo ./Cargo.lock) ];
 
   meta = with lib; {
     description = "The CLI text viewer tool that works like less command on small pane within the terminal window";
