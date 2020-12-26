736c00fcb1b17a4545612cb3b1ec29d4e0976d18
Alyssa Ross <hi@alyssa.is>
2020-12-26 23:54:10 +0000

    rargs: convert to importCargo

--- pkgs/tools/misc/rargs/default.nix
+++ pkgs/tools/misc/rargs/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, rustPlatform, fetchFromGitHub }:
+{ stdenv, rustPlatform, fetchFromGitHub, importCargo }:
 
 rustPlatform.buildRustPackage rec {
   pname   = "rargs";
@@ -11,7 +11,7 @@ rustPlatform.buildRustPackage rec {
     sha256 = "188gj05rbivci1z4z29vwdwxlj2w01v5i4avwrxjnj1dd6mmlbxd";
   };
 
-  cargoSha256 = "0qzkhx0n28f5wy4fral3adn499q3f10q71cd544s4ghqwqn4khc9";
+  nativeBuildInputs = [ (importCargo ./Cargo.lock) ];
 
   doCheck=false;  # `rargs`'s test depends on the deprecated `assert_cli` crate, which in turn is not in Nixpkgs
 
