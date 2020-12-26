b3a050892d91d5f5342536e7753f0788e8faa09a
Alyssa Ross <hi@alyssa.is>
2020-12-26 22:57:35 +0000

    tealdeer: convert to importCargo

--- pkgs/tools/misc/tealdeer/default.nix
+++ pkgs/tools/misc/tealdeer/default.nix
@@ -1,5 +1,6 @@
 { stdenv
 , rustPlatform
+, importCargo
 , fetchFromGitHub
 , pkg-config
 , installShellFiles
@@ -18,11 +19,10 @@ rustPlatform.buildRustPackage rec {
     sha256 = "1f37qlw4nxdhlqlqzzb4j11gsv26abk2nk2qhbzj77kp4v2b125x";
   };
 
-  cargoSha256 = "0g5fjj677qzhw3nw7f3n5gghsj2y811bdclxpy8aq2n58gbwvhvc";
-
   buildInputs = if stdenv.isDarwin then [ Security ] else [ openssl ];
-
-  nativeBuildInputs = [ installShellFiles pkg-config ];
+  nativeBuildInputs = [
+    installShellFiles pkg-config (importCargo ./Cargo.lock)
+  ];
 
   postInstall = ''
     installShellCompletion --bash --name tealdeer.bash bash_tealdeer
