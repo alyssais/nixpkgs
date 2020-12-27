5bb0d279f1b757a4c84102d1068a35afd7a450d4
Alyssa Ross <hi@alyssa.is>
2020-12-27 03:54:59 +0000

    viu: convert to importCargo

--- pkgs/tools/graphics/viu/default.nix
+++ pkgs/tools/graphics/viu/default.nix
@@ -1,4 +1,4 @@
-{ lib, rustPlatform, fetchFromGitHub }:
+{ lib, rustPlatform, fetchFromGitHub, importCargo }:
 
 rustPlatform.buildRustPackage rec {
   pname = "viu";
@@ -10,10 +10,11 @@ rustPlatform.buildRustPackage rec {
     rev = "v${version}";
     sha256 = "1q248jnbv64mkvg18465dpvjkw2v2hfqyvdvdixyrwyrnlv5cicv";
   };
+
+  nativeBuildInputs = [ (importCargo ./Cargo.lock) ];
+
   # tests are failing, reported at upstream: https://github.com/atanunq/viu/issues/40
   doCheck = false;
-
-  cargoSha256 = "18rskn8fchlgk295yk8sc2g1x6h43rmhqif871hgzdx1i35sbajr";
 
   meta = with lib; {
     description = "A command-line application to view images from the terminal written in Rust";
