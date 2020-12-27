ee4c99b640181adb00a7fc21bd1c00e05aecf3d2
Alyssa Ross <hi@alyssa.is>
2020-12-27 01:44:57 +0000

    heatseeker: convert to importCargo

--- pkgs/tools/misc/heatseeker/default.nix
+++ pkgs/tools/misc/heatseeker/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchFromGitHub, rustPlatform, coreutils }:
+{ stdenv, fetchFromGitHub, rustPlatform, importCargo, coreutils }:
 
 rustPlatform.buildRustPackage rec {
   pname = "heatseeker";
@@ -11,14 +11,14 @@ rustPlatform.buildRustPackage rec {
     sha256 = "1x7mdyf1m17s55f6yjdr1j510kb7a8f3zkd7lb2kzdc7nd3vgaxg";
   };
 
-  cargoSha256 = "0jnlcm7v29m4nc318qgf7r7jvs80s7n04fw83imm506vwr9rxbx9";
-
   # https://github.com/rschmitt/heatseeker/issues/42
   # I've suggested using `/usr/bin/env stty`, but doing that isn't quite as simple
   # as a substitution, and this works since we have the path to coreutils stty.
   patchPhase = ''
     substituteInPlace src/screen/unix.rs --replace "/bin/stty" "${coreutils}/bin/stty"
   '';
+
+  nativeBuildInputs = [ (importCargo ./Cargo.lock) ];
 
   # some tests require a tty, this variable turns them off for Travis CI,
   # which we can also make use of
