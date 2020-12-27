6e44a0f0ed98460b1514240f050afb7dc03e38dc
Alyssa Ross <hi@alyssa.is>
2020-12-27 01:28:23 +0000

    hyperfine: convert to importCargo

--- pkgs/tools/misc/hyperfine/default.nix
+++ pkgs/tools/misc/hyperfine/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchCrate, rustPlatform, installShellFiles
+{ stdenv, fetchCrate, rustPlatform, installShellFiles, importCargo
 , Security
 }:
 
@@ -11,9 +11,7 @@ rustPlatform.buildRustPackage rec {
     sha256 = "0dla2jzwcxkdx3n4fqkkh6wirqs2f31lvqsw2pjf1jbnnif54mzh";
   };
 
-  cargoSha256 = "12qdllhydd29xh20l5gir6qpj4a1nkzp8ics344rcwj8wsj7g5zw";
-
-  nativeBuildInputs = [ installShellFiles ];
+  nativeBuildInputs = [ installShellFiles (importCargo ./Cargo.lock) ];
   buildInputs = stdenv.lib.optional stdenv.isDarwin Security;
 
   postInstall = ''
