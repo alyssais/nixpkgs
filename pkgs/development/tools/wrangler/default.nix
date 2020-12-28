0a410ebd4a094062ca9c6ad17bbe93b49d3de104
Alyssa Ross <hi@alyssa.is>
2020-12-28 01:22:29 +0000

    wrangler: convert to importCargo

--- pkgs/development/tools/wrangler/default.nix
+++ pkgs/development/tools/wrangler/default.nix
@@ -1,4 +1,6 @@
-{ stdenv, fetchFromGitHub, rustPlatform, pkg-config, openssl, curl, darwin, perl }:
+{ stdenv, fetchFromGitHub, rustPlatform, importCargo
+, pkg-config, openssl, curl, darwin, perl
+}:
 
 rustPlatform.buildRustPackage rec {
   pname = "wrangler";
@@ -11,9 +13,8 @@ rustPlatform.buildRustPackage rec {
     sha256 = "1w0j6if1fnih1036hlb9a3c6wgjw4p057llhjf0f3d568ah1244a";
   };
 
-  cargoSha256 = "0d9wvdjjakznz8dnqx4gqxh0xkxrh4229460hg6dr9qn492p7nfx";
-
-  nativeBuildInputs = [ perl ] ++ stdenv.lib.optionals stdenv.isLinux [ pkg-config ];
+  nativeBuildInputs = [ perl (importCargo ./Cargo.lock) ]
+    ++ stdenv.lib.optionals stdenv.isLinux [ pkg-config ];
 
   buildInputs = stdenv.lib.optionals stdenv.isLinux [ openssl ]
     ++ stdenv.lib.optionals stdenv.isDarwin [
