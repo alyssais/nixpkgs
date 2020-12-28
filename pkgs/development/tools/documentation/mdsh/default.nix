bcec9348b23b23259d10113e7bba87ed3ddef2cb
Alyssa Ross <hi@alyssa.is>
2020-12-28 03:09:51 +0000

    mdsh: convert to importCargo

--- pkgs/development/tools/documentation/mdsh/default.nix
+++ pkgs/development/tools/documentation/mdsh/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchFromGitHub, rustPlatform }:
+{ stdenv, fetchFromGitHub, rustPlatform, importCargo }:
 
 rustPlatform.buildRustPackage rec {
   pname = "mdsh";
@@ -11,7 +11,7 @@ rustPlatform.buildRustPackage rec {
     sha256 = "02xslf5ssmyklbfsif2d7yk5aaz08n5w0dqiid6v4vlr2mkqcpjl";
   };
 
-  cargoSha256 = "118ykkqlf0x6gcgywx4pg3qawfhfr5q5f51gvrw9s302c1lmgk3g";
+  buildInputs = [ (importCargo ./Cargo.lock) ];
 
   meta = with stdenv.lib; {
     description = "Markdown shell pre-processor";
