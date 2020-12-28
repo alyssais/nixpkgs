1b88a2416f006a10039a8bd9249638d9d1657cb7
Alyssa Ross <hi@alyssa.is>
2020-12-28 00:43:04 +0000

    system76-firmware: convert to importCargo

--- pkgs/os-specific/linux/firmware/system76-firmware/default.nix
+++ pkgs/os-specific/linux/firmware/system76-firmware/default.nix
@@ -1,4 +1,7 @@
-{ rustPlatform, lib, fetchFromGitHub, lzma, pkgconfig, openssl, dbus, efibootmgr, makeWrapper }:
+{ rustPlatform, lib, fetchFromGitHub, importCargo
+, lzma, pkgconfig, openssl, dbus, efibootmgr, makeWrapper
+}:
+
 rustPlatform.buildRustPackage rec {
   pname = "system76-firmware";
   # Check Makefile when updating, make sure postInstall matches make install
@@ -11,13 +14,10 @@ rustPlatform.buildRustPackage rec {
     sha256 = "0yjv3a8r01ks91gc33rdwqmw52cqqwhq9f3rvw2xv3h8cqa5hfz0";
   };
 
-  nativeBuildInputs = [ pkgconfig makeWrapper ];
-
+  nativeBuildInputs = [ pkgconfig makeWrapper (importCargo ./Cargo.lock) ];
   buildInputs = [ lzma openssl dbus ];
 
   cargoBuildFlags = [ "--workspace" ];
-
-  cargoSha256 = "1ivn3i6kpnswiipqw5s67p6gsz3y6an0ahf6vwz7dlw2xaha0xbx";
 
   # Purposefully don't install systemd unit file, that's for NixOS
   postInstall = ''
