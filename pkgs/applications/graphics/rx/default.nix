8ba0bb03e63a012be4cb04274a520c6c1cee89fc
Alyssa Ross <hi@alyssa.is>
2020-12-28 19:15:26 +0000

    rx: convert to importCargo

--- pkgs/applications/graphics/rx/default.nix
+++ pkgs/applications/graphics/rx/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, rustPlatform, fetchFromGitHub, makeWrapper
+{ stdenv, rustPlatform, fetchFromGitHub, importCargo, makeWrapper
 , cmake, pkg-config
 , xorg ? null
 , libGL ? null }:
@@ -16,15 +16,13 @@ rustPlatform.buildRustPackage rec {
     sha256 = "1pln65pqy39ijrld11d06klwzfhhzmrgdaxijpx9q7w9z66zmqb8";
   };
 
-  cargoSha256 = "143a5x61s7ywk0ljqd10jkfvs6lrhlibkm2a9lw41wq13mgzb78j";
-
   nativeBuildInputs = [ cmake pkg-config makeWrapper ];
-
-  buildInputs = optionals stdenv.isLinux
-  (with xorg; [
-    # glfw-sys dependencies:
-    libX11 libXrandr libXinerama libXcursor libXi libXext
-  ]);
+  buildInputs = [ (importCargo ./Cargo.lock) ]
+    ++ optionals stdenv.isLinux
+      (with xorg; [
+        # glfw-sys dependencies:
+        libX11 libXrandr libXinerama libXcursor libXi libXext
+      ]);
 
   # FIXME: GLFW (X11) requires DISPLAY env variable for all tests
   doCheck = false;
