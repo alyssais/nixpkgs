685f27d0d4705dcc99356b2aabf46b39fe137281
Alyssa Ross <hi@alyssa.is>
2020-12-28 19:15:56 +0000

    whitebox-tools: convert to importCargo

--- pkgs/applications/gis/whitebox-tools/default.nix
+++ pkgs/applications/gis/whitebox-tools/default.nix
@@ -1,4 +1,5 @@
-{ stdenv, rustPlatform, fetchFromGitHub, Security }:
+{ stdenv, rustPlatform, fetchFromGitHub, importCargo, Security }:
+
 rustPlatform.buildRustPackage rec {
   pname = "whitebox_tools";
   version = "1.4.0";
@@ -10,9 +11,8 @@ rustPlatform.buildRustPackage rec {
     sha256 = "0s5byn8qyi1bm59j9vhwqaygw5cxipc7wbd3flh7n24nx0s8pr8c";
   };
 
-  buildInputs = stdenv.lib.optional stdenv.isDarwin Security;
-
-  cargoSha256 = "09rz8f1xyc64qjbj6pgw8jxr2a7chghmdc6sfkbv7hdvx6vg4wvk";
+  buildInputs = [ (importCargo ./Cargo.lock) ]
+    ++ stdenv.lib.optional stdenv.isDarwin Security;
 
   meta = with stdenv.lib; {
     description = "An advanced geospatial data analysis platform";
