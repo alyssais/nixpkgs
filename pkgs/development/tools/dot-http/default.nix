0043fdef43a09db52d01e7398158af43eb620ba6
Alyssa Ross <hi@alyssa.is>
2020-12-28 02:57:38 +0000

    dot-http: convert to importCargo

--- pkgs/development/tools/dot-http/default.nix
+++ pkgs/development/tools/dot-http/default.nix
@@ -1,4 +1,6 @@
-{ stdenv, rustPlatform, fetchFromGitHub, pkg-config, openssl, libiconv, Security }:
+{ stdenv, rustPlatform, fetchFromGitHub, importCargo
+, pkg-config, openssl, libiconv, Security
+}:
 
 rustPlatform.buildRustPackage rec {
   pname = "dot-http";
@@ -11,13 +13,9 @@ rustPlatform.buildRustPackage rec {
     sha256 = "1s2q4kdldhb5gd14g2h6vzrbjgbbbs9zp2dgmna0rhk1h4qv0mml";
   };
 
-  cargoSha256 = "0an3hskq1k2j4gdn8wvhfb9pqsc34ibs5bv7sjznkp5jma6fdr9w";
-
   nativeBuildInputs = [ pkg-config ];
-
-  buildInputs = [ openssl ] ++ stdenv.lib.optionals stdenv.isDarwin [
-    libiconv Security
-  ];
+  buildInputs = [ openssl (importCargo ./Cargo.lock) ]
+    ++ stdenv.lib.optionals stdenv.isDarwin [ libiconv Security ];
 
   meta = with stdenv.lib; {
     description = "Text-based scriptable HTTP client";
