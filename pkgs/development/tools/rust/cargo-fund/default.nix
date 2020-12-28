d83ad63130b90008f42dd0ff836faad907ec8810
Alyssa Ross <hi@alyssa.is>
2020-12-28 01:58:42 +0000

    cargo-fund: convert to importCargo

--- pkgs/development/tools/rust/cargo-fund/default.nix
+++ pkgs/development/tools/rust/cargo-fund/default.nix
@@ -1,4 +1,6 @@
-{ stdenv, fetchFromGitHub, pkg-config, rustPlatform, Security, curl, openssl, libiconv }:
+{ stdenv, fetchFromGitHub, pkg-config, rustPlatform, importCargo
+, Security, curl, openssl, libiconv
+}:
 
 rustPlatform.buildRustPackage rec {
   pname = "cargo-fund";
@@ -11,14 +13,12 @@ rustPlatform.buildRustPackage rec {
     sha256 = "1jim5bgq3fc33391qpa1q1csbzqf4hk1qyfzwxpcs5pb4ixb6vgk";
   };
 
-  cargoSha256 = "181gcmaw2w5a6ah8a2ahsnc1zkadpmx1azkwh2a6x8myhzw2dxsj";
-
   # The tests need a GitHub API token.
   doCheck = false;
 
   nativeBuildInputs = [ pkg-config ];
-
-  buildInputs = [ openssl ] ++ stdenv.lib.optionals stdenv.isDarwin [ Security libiconv curl ];
+  buildInputs = [ openssl (importCargo ./Cargo.lock) ]
+    ++ stdenv.lib.optionals stdenv.isDarwin [ Security libiconv curl ];
 
   meta = with stdenv.lib; {
     description = "Discover funding links for your project's dependencies";
