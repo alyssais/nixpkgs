c288635dca93d235d019888eb0ce34f2e8a57c35
Alyssa Ross <hi@alyssa.is>
2020-12-28 03:25:48 +0000

    svlint: convert to importCargo

--- pkgs/development/tools/analysis/svlint/default.nix
+++ pkgs/development/tools/analysis/svlint/default.nix
@@ -1,5 +1,6 @@
 { lib
 , rustPlatform
+, importCargo
 , fetchFromGitHub
 }:
 
@@ -14,7 +15,7 @@ rustPlatform.buildRustPackage rec {
     sha256 = "1anbanvaikj8g5jcmjd165krzvjdwj1cl8f3vm1nmy997x8vxihf";
   };
 
-  cargoSha256 = "18xnqsrnf2gc9zny8ayzf1bah8mq1fy5k807cz039gqz36cvnma0";
+  buildInputs = [ (importCargo ./Cargo.lock) ];
 
   meta = with lib; {
     description = "SystemVerilog linter";
