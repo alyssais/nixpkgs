46638f06daad691a2683ac87bdaeb2ca470ece40
Alyssa Ross <hi@alyssa.is>
2020-12-28 02:36:03 +0000

    cargo-bloat: convert to importCargo

--- pkgs/development/tools/rust/cargo-bloat/default.nix
+++ pkgs/development/tools/rust/cargo-bloat/default.nix
@@ -1,4 +1,4 @@
-{ lib, rustPlatform, fetchFromGitHub }:
+{ lib, rustPlatform, fetchFromGitHub, importCargo }:
 
 rustPlatform.buildRustPackage rec {
   pname = "cargo-bloat";
@@ -11,7 +11,7 @@ rustPlatform.buildRustPackage rec {
     sha256 = "0bqzzh8vfqm7dpnb0fv4calnhsg9p3c5j06ycvg621p4zp4fydh2";
   };
 
-  cargoSha256 = "1323lcl8fa21pgx3jhwl4w9f8qz3jjxb5qdvib9jdzqxnnw320xs";
+  buildInputs = [ (importCargo ./Cargo.lock) ];
 
   meta = with lib; {
     description = "A tool and Cargo subcommand that helps you find out what takes most of the space in your executable";
