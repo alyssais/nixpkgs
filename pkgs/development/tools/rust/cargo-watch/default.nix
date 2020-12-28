c88935a014314aa1111930fddc498d0c61e80094
Alyssa Ross <hi@alyssa.is>
2020-12-28 01:49:32 +0000

    cargo-watch: convert to importCargo

--- pkgs/development/tools/rust/cargo-watch/default.nix
+++ pkgs/development/tools/rust/cargo-watch/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, lib, rustPlatform, fetchFromGitHub, CoreServices, rust }:
+{ stdenv, lib, rustPlatform, fetchFromGitHub, importCargo, CoreServices, rust }:
 
 rustPlatform.buildRustPackage rec {
   pname = "cargo-watch";
@@ -11,8 +11,7 @@ rustPlatform.buildRustPackage rec {
     sha256 = "181v922nx8152ymszig1kw6y0mnix6si1zfnv6vqgr5r53pwkbc1";
   };
 
-  cargoSha256 = "1abhzi1396x8lnxvakj34nysl3ybvsaf21j678wzaqwj16sd3w3d";
-
+  nativeBuildInputs = [ (importCargo ./Cargo.lock) ];
   buildInputs = lib.optional stdenv.isDarwin CoreServices;
 
   # `test with_cargo` tries to call cargo-watch as a cargo subcommand
