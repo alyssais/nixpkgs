ea2cda982fca143bc0dc596e1bf0c6d4c4670f78
Alyssa Ross <hi@alyssa.is>
2020-12-29 03:23:46 +0000

    rdedup: convert to importCargo

--- pkgs/tools/backup/rdedup/default.nix
+++ pkgs/tools/backup/rdedup/default.nix
@@ -1,6 +1,6 @@
-{ stdenv, fetchFromGitHub, rustPlatform, pkgconfig, openssl, libsodium
-, llvmPackages, clang, lzma
-, Security }:
+{ stdenv, fetchFromGitHub, rustPlatform, importCargo
+, pkgconfig, openssl, libsodium, llvmPackages, clang, lzma, Security
+}:
 
 rustPlatform.buildRustPackage rec {
   pname = "rdedup";
@@ -13,14 +13,8 @@ rustPlatform.buildRustPackage rec {
     sha256 = "0y34a3mpghdmcb2rx4z62q0s351bfmy1287d75mm07ryfgglgsd7";
   };
 
-  cargoSha256 = "0akwb7ak4h1i1zk4wcn27zyqjz6mrchs47014xbzw22rj8h8dx92";
-
-  cargoPatches = [
-    ./v3.1.1-fix-Cargo.lock.patch
-  ];
-
   nativeBuildInputs = [ pkgconfig llvmPackages.libclang clang ];
-  buildInputs = [ openssl libsodium lzma ]
+  buildInputs = [ (importCargo ./Cargo.lock) openssl libsodium lzma ]
     ++ (stdenv.lib.optional stdenv.isDarwin Security);
 
   configurePhase = ''
