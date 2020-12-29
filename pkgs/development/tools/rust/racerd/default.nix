8768056282ed5a9999b9be048d008ad28841f3d9
Alyssa Ross <hi@alyssa.is>
2020-12-29 06:08:18 +0000

    rustracerd: convert to importCargo

--- pkgs/development/tools/rust/racerd/default.nix
+++ pkgs/development/tools/rust/racerd/default.nix
@@ -1,4 +1,6 @@
-{ stdenv, fetchFromGitHub, fetchpatch, rustPlatform, makeWrapper, Security }:
+{ stdenv, fetchFromGitHub, fetchpatch, rustPlatform, importCargo
+, makeWrapper, Security
+}:
 
 rustPlatform.buildRustPackage rec {
   pname = "racerd";
@@ -11,22 +13,14 @@ rustPlatform.buildRustPackage rec {
     sha256 = "13jqdvjk4savcl03mrn2vzgdsd7vxv2racqbyavrxp2cm9h6cjln";
   };
 
-  cargoPatches = [
-    (fetchpatch {
-      url = "https://github.com/jwilm/racerd/commit/856f3656e160cd2909c5166e962f422c901720ee.patch";
-      sha256 = "1qq2k4bnwjz5qgn7s8yxd090smwn2wvdm8dd1rrlgpln0a5vxkpb";
-    })
-  ];
-
-  cargoSha256 = "1z0dh2j9ik66i6nww3z7z2gw7nhc0b061zxbjzamk1jybpc845lq";
-
   # a nightly compiler is required unless we use this cheat code.
   RUSTC_BOOTSTRAP=1;
 
   doCheck = false;
 
   nativeBuildInputs = [ makeWrapper ];
-  buildInputs = stdenv.lib.optional stdenv.isDarwin Security;
+  buildInputs = [ (importCargo ./Cargo.lock) ]
+    ++ stdenv.lib.optional stdenv.isDarwin Security;
 
   RUST_SRC_PATH = rustPlatform.rustcSrc;
 
