alyssais/nixpkgs

commit fb2fa1b50fe32e4d4f4e251b5892598f38f40a71

nixos/postfix: pull setup into its own unit
Consider a service that generates postfix lookup tables with
postmap(1), like Mailman.  It needs the Postfix configuration file to
exist, but Postfix qmgr needs all the lookup tables its configured
with to exist before it starts.  So the service that runs postmap
needs to run after the Postfix configuration and directory structure
is generated, but before Postfix itself is started.  To enable this,
we split Postfix into two units: a oneshot unit that sets up the
configuration, and a longrun unit that supervises the Postfix
daemons.  The postmap services can then be inserted in between these
two units.

nixos/modules/services/mail/postfix.nix
@@ -723,23 +723,10 @@ in
         { ${setgidGroup}.gid = config.ids.gids.postdrop;
         };
 
-      systemd.services.postfix =
-        { description = "Postfix mail server";
-
-          wantedBy = [ "multi-user.target" ];
-          after = [ "network.target" ];
-          path = [ pkgs.postfix ];
-
-          serviceConfig = {
-            Type = "forking";
-            Restart = "always";
-            PIDFile = "/var/lib/postfix/queue/pid/master.pid";
-            ExecStart = "${pkgs.postfix}/bin/postfix start";
-            ExecStop = "${pkgs.postfix}/bin/postfix stop";
-            ExecReload = "${pkgs.postfix}/bin/postfix reload";
-          };
-
-          preStart = ''
+      systemd.services.postfix-setup =
+        { description = "Setup for Postfix mail server";
+          serviceConfig.Type = "oneshot";
+          script = ''
             # Backwards compatibility
             if [ ! -d /var/lib/postfix ] && [ -d /var/postfix ]; then
               mkdir -p /var/lib
@@ -775,6 +762,24 @@ in
             #Finally delegate to postfix checking remain directories in /var/lib/postfix and set permissions on them
             ${pkgs.postfix}/bin/postfix set-permissions config_directory=/var/lib/postfix/conf
           '';
+        };
+
+      systemd.services.postfix =
+        { description = "Postfix mail server";
+
+          wantedBy = [ "multi-user.target" ];
+          after = [ "network.target" "postfix-setup.service" ];
+          requires = [ "postfix-setup.service" ];
+          path = [ pkgs.postfix ];
+
+          serviceConfig = {
+            Type = "forking";
+            Restart = "always";
+            PIDFile = "/var/lib/postfix/queue/pid/master.pid";
+            ExecStart = "${pkgs.postfix}/bin/postfix start";
+            ExecStop = "${pkgs.postfix}/bin/postfix stop";
+            ExecReload = "${pkgs.postfix}/bin/postfix reload";
+          };
         };
 
       services.postfix.config = (mapAttrs (_: v: mkDefault v) {

nixos/modules/services/mail/mailman.nix
@@ -313,7 +313,8 @@ in {
     systemd.services = {
       mailman = {
         description = "GNU Mailman Master Process";
-        after = [ "network.target" ];
+        after = [ "network.target" ]
+          ++ lib.optional cfg.enablePostfix "postfix-setup.service";
         restartTriggers = [ config.environment.etc."mailman.cfg".source ];
         wantedBy = [ "multi-user.target" ];
         serviceConfig = {

commit 0c66e7449f350f7ff68628a07e28a06bc689fce0

nixos/postfix: support reloading configuration
When I split the postfix setup out into its own unit, I introduced a
bug — now, when Postfix's configuration changes as part of a
NixOS system switch, Postfix won't see the new configuration unless
the service is manually restarted.  This happens because only
postfix-setup.service changes, not postfix.service.

To fix this, I've added changed postfix-setup.service to reload when
it's changed (rather than restarting), and told postfix.service to
reload when postfix-setup.service is also reloaded.  This has the
additional advantage over restarting that this way the daemon can just
keep running, with no service interruption.

Fixes: fb2fa1b50fe ("nixos/postfix: pull setup into its own unit")

nixos/modules/services/mail/postfix.nix
@@ -254,6 +254,27 @@ let
   transportFile = pkgs.writeText "postfix-transport" cfg.transport;
   headerChecksFile = pkgs.writeText "postfix-header-checks" headerChecks;
 
+  linkPostfixConfig = pkgs.writeShellScript "link-postfix-config.sh" ''
+    rm -rf /var/lib/postfix/conf
+    mkdir -p /var/lib/postfix/conf
+    chmod 0755 /var/lib/postfix/conf
+    ln -sf ${pkgs.postfix}/etc/postfix/postfix-files /var/lib/postfix/conf/postfix-files
+    ln -sf ${mainCfFile} /var/lib/postfix/conf/main.cf
+    ln -sf ${masterCfFile} /var/lib/postfix/conf/master.cf
+
+    ${concatStringsSep "\n" (mapAttrsToList (to: from: ''
+      ln -sf ${from} /var/lib/postfix/conf/${to}
+      ${pkgs.postfix}/bin/postalias /var/lib/postfix/conf/${to}
+    '') cfg.aliasFiles)}
+    ${concatStringsSep "\n" (mapAttrsToList (to: from: ''
+      ln -sf ${from} /var/lib/postfix/conf/${to}
+      ${pkgs.postfix}/bin/postmap /var/lib/postfix/conf/${to}
+    '') cfg.mapFiles)}
+
+    #Finally delegate to postfix checking remain directories in /var/lib/postfix and set permissions on them
+    ${pkgs.postfix}/bin/postfix set-permissions config_directory=/var/lib/postfix/conf
+  '';
+
 in
 
 {
@@ -725,6 +746,8 @@ in
 
       systemd.services.postfix-setup =
         { description = "Setup for Postfix mail server";
+          reloadIfChanged = true;
+          serviceConfig.ExecReload = linkPostfixConfig;
           serviceConfig.RemainAfterExit = true;
           serviceConfig.Type = "oneshot";
           script = ''
@@ -739,29 +762,12 @@ in
             chmod 0755 /var/lib/postfix
             chown root:root /var/lib/postfix
 
-            rm -rf /var/lib/postfix/conf
-            mkdir -p /var/lib/postfix/conf
-            chmod 0755 /var/lib/postfix/conf
-            ln -sf ${pkgs.postfix}/etc/postfix/postfix-files /var/lib/postfix/conf/postfix-files
-            ln -sf ${mainCfFile} /var/lib/postfix/conf/main.cf
-            ln -sf ${masterCfFile} /var/lib/postfix/conf/master.cf
-
-            ${concatStringsSep "\n" (mapAttrsToList (to: from: ''
-              ln -sf ${from} /var/lib/postfix/conf/${to}
-              ${pkgs.postfix}/bin/postalias /var/lib/postfix/conf/${to}
-            '') cfg.aliasFiles)}
-            ${concatStringsSep "\n" (mapAttrsToList (to: from: ''
-              ln -sf ${from} /var/lib/postfix/conf/${to}
-              ${pkgs.postfix}/bin/postmap /var/lib/postfix/conf/${to}
-            '') cfg.mapFiles)}
-
             mkdir -p /var/spool/mail
             chown root:root /var/spool/mail
             chmod a+rwxt /var/spool/mail
             ln -sf /var/spool/mail /var/
 
-            #Finally delegate to postfix checking remain directories in /var/lib/postfix and set permissions on them
-            ${pkgs.postfix}/bin/postfix set-permissions config_directory=/var/lib/postfix/conf
+            ${linkPostfixConfig}
           '';
         };
 
@@ -780,6 +786,10 @@ in
             ExecStart = "${pkgs.postfix}/bin/postfix start";
             ExecStop = "${pkgs.postfix}/bin/postfix stop";
             ExecReload = "${pkgs.postfix}/bin/postfix reload";
+          };
+
+          unitConfig = {
+            ReloadPropagatedFrom = "postfix-setup.service";
           };
         };
 