alyssais/nixpkgs

commit 7cdaa0b80c9da64b2acb6cc8e8f73d2f4489aaeb

protobuf: protobuf_25 -> protobuf_28
Bump default protobuf from 25.4 to 28.0, as well as the corresponding
python package.

Changelog:
https://github.com/protocolbuffers/protobuf/releases/tag/v28.0

Diff: https://github.com/protocolbuffers/protobuf/compare/v25.4...v28.0

pkgs/top-level/python-packages.nix
@@ -10894,7 +10894,7 @@ self: super: with self; {
   protobuf5 = callPackage ../development/python-modules/protobuf/default.nix { };
 
   # If a protobuf upgrade causes many Python packages to fail, please pin it here to the previous version.
-  protobuf = protobuf4;
+  protobuf = protobuf5;
 
   protobuf3-to-dict = callPackage ../development/python-modules/protobuf3-to-dict { };
 

pkgs/development/libraries/protobuf/generic.nix
@@ -28,7 +28,7 @@ stdenv.mkDerivation (finalAttrs: {
   src = fetchFromGitHub {
     owner = "protocolbuffers";
     repo = "protobuf";
-    rev = "v${version}";
+    rev = "refs/tags/v${version}";
     inherit hash;
   };
 
@@ -89,9 +89,7 @@ stdenv.mkDerivation (finalAttrs: {
 
   passthru = {
     tests = {
-      pythonProtobuf = python3.pkgs.protobuf.override (_: {
-        protobuf = finalAttrs.finalPackage;
-      });
+      pythonProtobuf = python3.pkgs.protobuf;
       inherit grpc;
     };
 

pkgs/top-level/all-packages.nix
@@ -23147,7 +23147,7 @@ with pkgs;
 
   prospector = callPackage ../development/tools/prospector { };
 
-  protobuf = protobuf_25;
+  protobuf = protobuf_28;
 
   inherit
     ({

commit 9dd80db828ac9154abb802c01eb2ead6a7c88eec

python3Packages.protobuf4: pin to protobuf_25
This is the most recent protobuf version supported.  With more recent
versions it's correctly marked broken.

Fixes: 7cdaa0b80c9d ("protobuf: protobuf_25 -> protobuf_28")

pkgs/top-level/python-packages.nix
@@ -10580,7 +10580,7 @@ self: super: with self; {
 
   # Protobuf 4.x
   protobuf4 = callPackage ../development/python-modules/protobuf/4.nix {
-    protobuf = pkgs.protobuf;
+    protobuf = pkgs.protobuf_25;
   };
 
   # Protobuf 5.x