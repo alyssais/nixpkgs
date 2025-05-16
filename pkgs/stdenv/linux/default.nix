08314322cc8596000b683f9775dc01c82d2a3b79
Alyssa Ross <hi@alyssa.is>
2025-05-16 12:04:30 +0200

    pkgs/stdenv/linux: update riscv64-unknown-linux-musl bootstrap-files
    sha256sum of files to be uploaded:
    
    $ sha256sum /nix/store/n8cgi0w25w04m0p0609d2wbxxc80ac61-stdenv-bootstrap-tools-riscv64-unknown-linux-musl/on-server/*
    5dd9a613cd74814741fee43dfd0acb50f070427b79b38d708d500d75b863bcbb  /nix/store/n8cgi0w25w04m0p0609d2wbxxc80ac61-stdenv-bootstrap-tools-riscv64-unknown-linux-musl/on-server/bootstrap-tools.tar.xz
    ae9426ad88903f3048b101f67f48c0f65a80dfe3f19930e7955145b40f2871db  /nix/store/n8cgi0w25w04m0p0609d2wbxxc80ac61-stdenv-bootstrap-tools-riscv64-unknown-linux-musl/on-server/busybox
    
    Suggested commands to upload files to 'tarballs.nixos.org':
    
        $ nix-store --realize /nix/store/n8cgi0w25w04m0p0609d2wbxxc80ac61-stdenv-bootstrap-tools-riscv64-unknown-linux-musl
        $ aws s3 cp --recursive --acl public-read /nix/store/n8cgi0w25w04m0p0609d2wbxxc80ac61-stdenv-bootstrap-tools-riscv64-unknown-linux-musl/on-server/ s3://nixpkgs-tarballs/stdenv/riscv64-unknown-linux-musl/7d78e04d4d13ce0b5a97ea52d4debf052756c30d
        $ aws s3 cp --recursive s3://nixpkgs-tarballs/stdenv/riscv64-unknown-linux-musl/7d78e04d4d13ce0b5a97ea52d4debf052756c30d ./
        $ sha256sum bootstrap-tools.tar.xz busybox
        $ sha256sum /nix/store/n8cgi0w25w04m0p0609d2wbxxc80ac61-stdenv-bootstrap-tools-riscv64-unknown-linux-musl/on-server/*

--- pkgs/stdenv/linux/default.nix
+++ pkgs/stdenv/linux/default.nix
@@ -87,6 +87,7 @@
         musl = {
           aarch64-linux = import ./bootstrap-files/aarch64-unknown-linux-musl.nix;
           armv6l-linux = import ./bootstrap-files/armv6l-unknown-linux-musleabihf.nix;
+          riscv64-linux = import ./bootstrap-files/riscv64-unknown-linux-musl.nix;
           x86_64-linux = import ./bootstrap-files/x86_64-unknown-linux-musl.nix;
         };
       };
